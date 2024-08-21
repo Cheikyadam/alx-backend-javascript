154830f0a3fb0ed8986b7083a9fb20ddf36b0b74
Cheikyadam <urbaingande@gmail.com>
2024-08-21 11:04:53 +0100

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,9 +3,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.stdin.end();
+  process.exit();
 });
 
-process.stdin.on('end', () => {
+process.on('exit', () => {
   console.log('This important software is now closing');
 });
